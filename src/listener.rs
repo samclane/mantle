9b0ddc742a18c43f9186b0b897ca84d6e8b0ceab
Sawyer McLane <samclane@gmail.com>
2024-09-15 06:37:35 -0600

    Refactor InputListener to use shared input state

--- src/listener.rs
+++ src/listener.rs
@@ -1,129 +1,155 @@
-use log::error;
 use rdev::{listen, Event, EventType};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
 
 type Callback = Box<dyn Fn(Event) + Send>;
 
+pub struct SharedInputState {
+    last_mouse_position: Mutex<Option<(i32, i32)>>,
+    last_click_time: Mutex<Option<Instant>>,
+    button_pressed: Mutex<Option<rdev::Button>>,
+    last_button_pressed: Mutex<Option<rdev::Button>>,
+    keys_pressed: Mutex<Vec<rdev::Key>>,
+    last_keys_pressed: Mutex<Vec<rdev::Key>>,
+    callbacks: Mutex<Vec<Callback>>,
+}
+
+impl SharedInputState {
+    fn new() -> Self {
+        SharedInputState {
+            last_mouse_position: Mutex::new(None),
+            last_click_time: Mutex::new(None),
+            button_pressed: Mutex::new(None),
+            last_button_pressed: Mutex::new(None),
+            keys_pressed: Mutex::new(Vec::new()),
+            last_keys_pressed: Mutex::new(Vec::new()),
+            callbacks: Mutex::new(Vec::new()),
+        }
+    }
+
+    fn update_mouse_position(&self, x: i32, y: i32) {
+        if let Ok(mut pos) = self.last_mouse_position.lock() {
+            *pos = Some((x, y));
+        }
+    }
+
+    fn update_button_press(&self, button: rdev::Button) {
+        if let Ok(mut pressed) = self.button_pressed.lock() {
+            *pressed = Some(button);
+        }
+        if let Ok(mut time) = self.last_click_time.lock() {
+            *time = Some(Instant::now());
+        }
+        if let Ok(mut last) = self.last_button_pressed.lock() {
+            *last = Some(button);
+        }
+    }
+
+    fn update_key_press(&self, key: rdev::Key) {
+        if let Ok(mut keys) = self.keys_pressed.lock() {
+            keys.push(key);
+            if let Ok(mut last) = self.last_keys_pressed.lock() {
+                *last = keys.clone();
+            }
+        }
+    }
+
+    fn update_key_release(&self, key: rdev::Key) {
+        if let Ok(mut keys) = self.keys_pressed.lock() {
+            keys.retain(|&k| k != key);
+        }
+    }
+
+    fn update_button_release(&self) {
+        if let Ok(mut pressed) = self.button_pressed.lock() {
+            *pressed = None;
+        }
+    }
+
+    fn execute_callbacks(&self, event: &Event) {
+        if let Ok(callbacks) = self.callbacks.lock() {
+            for callback in callbacks.iter() {
+                callback(event.clone());
+            }
+        }
+    }
+
+    fn add_callback(&self, callback: Callback) {
+        if let Ok(mut callbacks) = self.callbacks.lock() {
+            callbacks.push(callback);
+        }
+    }
+}
+
 pub struct InputListener {
-    pub sender: mpsc::Sender<Event>,
-    pub receiver: Arc<Mutex<mpsc::Receiver<Event>>>,
-    pub callbacks: Arc<Mutex<Vec<Callback>>>,
-    last_mouse_position: Arc<Mutex<Option<(i32, i32)>>>,
-    last_click_time: Arc<Mutex<Option<Instant>>>,
-    button_pressed: Arc<Mutex<Option<rdev::Button>>>,
-    last_button_pressed: Arc<Mutex<Option<rdev::Button>>>,
-    keys_pressed: Arc<Mutex<Vec<rdev::Key>>>,
-    last_keys_pressed: Arc<Mutex<Vec<rdev::Key>>>,
+    state: Arc<SharedInputState>,
 }
 
 impl InputListener {
     pub fn new() -> Self {
-        let (sender, receiver) = mpsc::channel();
         InputListener {
-            sender,
-            receiver: Arc::new(Mutex::new(receiver)),
-            callbacks: Arc::new(Mutex::new(Vec::new())),
-            last_mouse_position: Arc::new(Mutex::new(None)),
-            last_click_time: Arc::new(Mutex::new(None)),
-            button_pressed: Arc::new(Mutex::new(None)),
-            last_button_pressed: Arc::new(Mutex::new(None)),
-            keys_pressed: Arc::new(Mutex::new(Vec::new())),
-            last_keys_pressed: Arc::new(Mutex::new(Vec::new())),
+            state: Arc::new(SharedInputState::new()),
         }
     }
 
     pub fn get_last_mouse_position(&self) -> Option<(i32, i32)> {
-        let pos = self.last_mouse_position.lock().unwrap();
-        *pos
+        *self.state.last_mouse_position.lock().unwrap()
     }
 
     pub fn get_last_click_time(&self) -> Option<Instant> {
-        let time = self.last_click_time.lock().unwrap();
-        *time
+        *self.state.last_click_time.lock().unwrap()
     }
 
     pub fn get_last_button_pressed(&self) -> Option<rdev::Button> {
-        let last = self.last_button_pressed.lock().unwrap();
-        *last
+        *self.state.last_button_pressed.lock().unwrap()
     }
 
     pub fn is_button_pressed(&self, button: rdev::Button) -> bool {
-        let pressed = self.button_pressed.lock().unwrap();
-        match *pressed {
+        match *self.state.button_pressed.lock().unwrap() {
             Some(b) => b == button,
             None => false,
         }
     }
 
     pub fn is_key_pressed(&self, key: rdev::Key) -> bool {
-        let keys = self.keys_pressed.lock().unwrap();
-        keys.contains(&key)
+        self.state.keys_pressed.lock().unwrap().contains(&key)
     }
 
     pub fn get_keys_pressed(&self) -> Vec<rdev::Key> {
-        let keys = self.keys_pressed.lock().unwrap();
-        keys.clone()
+        self.state.keys_pressed.lock().unwrap().clone()
     }
 
-    pub fn add_callback(&mut self, callback: Callback) {
-        let mut callbacks = self.callbacks.lock().unwrap();
-        callbacks.push(callback);
+    pub fn add_callback(&self, callback: Callback) {
+        self.state.add_callback(callback);
     }
 
     pub fn spawn(&self) -> thread::JoinHandle<()> {
-        let sender = self.sender.clone();
-        let callbacks = Arc::clone(&self.callbacks);
-        let last_mouse_position = Arc::clone(&self.last_mouse_position);
-        let last_click_time = Arc::clone(&self.last_click_time);
-        let button_pressed = Arc::clone(&self.button_pressed);
-        let last_button_pressed = Arc::clone(&self.last_button_pressed);
-        let keys_pressed = Arc::clone(&self.keys_pressed);
-        let last_keys_pressed = Arc::clone(&self.last_keys_pressed);
+        let state = Arc::clone(&self.state);
 
         thread::spawn(move || {
             listen(move |event| {
                 match event.event_type {
                     EventType::MouseMove { x, y } => {
-                        let mut pos = last_mouse_position.lock().unwrap();
-                        *pos = Some((x as i32, y as i32));
+                        state.update_mouse_position(x as i32, y as i32);
                     }
                     EventType::ButtonPress(button) => {
-                        let mut pressed = button_pressed.lock().unwrap();
-                        *pressed = Some(button);
-                        let mut time = last_click_time.lock().unwrap();
-                        *time = Some(Instant::now());
-                        let mut last = last_button_pressed.lock().unwrap();
-                        *last = Some(button);
+                        state.update_button_press(button);
                     }
                     EventType::KeyPress(key) => {
-                        let mut keys = keys_pressed.lock().unwrap();
-                        keys.push(key);
-                        let mut last = last_keys_pressed.lock().unwrap();
-                        *last = keys.clone();
+                        state.update_key_press(key);
                     }
                     EventType::KeyRelease(key) => {
-                        let mut keys = keys_pressed.lock().unwrap();
-                        keys.retain(|&k| k != key);
+                        state.update_key_release(key);
                     }
-                    EventType::ButtonRelease(_button) => {
-                        let mut pressed = button_pressed.lock().unwrap();
-                        *pressed = None;
+                    EventType::ButtonRelease(_) => {
+                        state.update_button_release();
                     }
                     _ => {}
                 }
 
                 // Execute all registered callbacks
-                let callbacks = callbacks.lock().unwrap();
-                for callback in callbacks.iter() {
-                    callback(event.clone());
-                }
-
-                // Send the event through the channel
-                sender
-                    .send(event)
-                    .unwrap_or_else(|e| error!("Could not send event {:?}", e));
+                state.execute_callbacks(&event);
             })
             .expect("Could not listen");
         })
