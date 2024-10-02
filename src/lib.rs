ceb9c9a75b53672f1948f3cddc9fb38c4d14b886
Sawyer McLane <samclane@gmail.com>
2024-10-02 10:48:07 -0600

    manager.rs -> device_manager.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod app;
 pub mod color;
 pub mod device_info;
+pub mod device_manager;
 pub mod listener;
-pub mod manager;
 pub mod products;
 pub mod refreshable_data;
 pub mod screencap;
@@ -12,7 +12,7 @@ pub mod utils;
 
 pub use color::{contrast_color, HSBK32, RGB8};
 pub use device_info::{BulbInfo, DeviceColor};
-pub use manager::LifxManager;
+pub use device_manager::LifxManager;
 pub use products::{get_products, Product};
 pub use refreshable_data::RefreshableData;
 pub use screencap::ScreencapManager;
